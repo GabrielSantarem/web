7a03ff06bfc9c5b12e22f2e9c9af5e1a7ae600c2
MrTomatePNG <mrtomate360@outlook.com>
2026-09-06 14:49:53 -0400

    novos handles :
    "/{$}": usa o wildercard `{$}` pra matching exato pra home
    "/me": devole um objeto json Me
    "/": handle pra NotFound, o padrao `/` casa com qualquer coisa e
    redireciona pro root  `/`
    
    "/favicon.ico": um pequeno teste de http.FileServe, ele ler de assets e
    cuida de cabeçalhos complexos como Tipo Mime e Content-Type corretos

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handler.HelloHandler())
+	mux.HandleFunc("GET /{$}", handler.HandleHello())
+	mux.HandleFunc("/me/{Name}", handler.HandleMe())
+	mux.HandleFunc("/", handler.HandleNotFound())
+
+	mux.HandleFunc("GET /favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "assets/favicon.ico")
+	})
 
 	http.ListenAndServe(":8080", mux)
 }
