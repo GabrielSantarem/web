bb42146ddec6964f6f375c78ca862ad7ab1f21a9
MrTomatePNG <mrtomate360@outlook.com>
2026-09-06 14:44:39 -0400

    HandleMe: extrai paramentros de rota e devolve uma struct `ME` no corpo json

--- handler/hello.go
+++ handler/hello.go
@@ -18,3 +18,24 @@ func HelloHandler() http.HandlerFunc {
 		json.NewEncoder(w).Encode(s)
 	}
 }
+
+func HandleMe() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		name := r.PathValue("Name")
+
+		me := types.Me{
+			Name: name,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(me)
+	}
+}
+
+func HandleNotFound() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
