89461fc0f580944af7f889c7896ec8682e2a0a9e
MrTomatePNG <mrtomate360@outlook.com>
2026-09-07 20:33:30 -0400

    user.go define nossas tipos do dominio
    UserRepository: Porta de saida, indica as operaçoes basicas que o
    dominio precisa pra persistir o User.
    UserService: Porta de entrada, expoe os servicos acessiveis ao mundo
    externo.
    userService: implementa as operações de serviço para usuários atuando
    como intermediário entre o repositório e o mundo externo.

---
+++ internal/adapters/inMemoryUserRepository.go
@@ -0,0 +1,36 @@
+package adapters
+
+import "codeberg.org/MrTomate/web/internal/core"
+
+type InMemoryUserRepository struct {
+	users []*core.User
+}
+
+func (r *InMemoryUserRepository) Save(u *core.User) (*core.User, error) {
+	r.users = append(r.users, u)
+	return u, nil
+}
+
+func (r *InMemoryUserRepository) FindById(id string) (*core.User, error) {
+	for _, u := range r.users {
+		if u.ID == id {
+			return u, nil
+		}
+	}
+	return nil, nil
+}
+
+func (r *InMemoryUserRepository) FindByEmail(email string) (*core.User, error) {
+	for _, u := range r.users {
+		if u.Email == email {
+			return u, nil
+		}
+	}
+	return nil, nil
+}
+
+func NewInMemoryUserRepository() *InMemoryUserRepository {
+	return &InMemoryUserRepository{
+		users: make([]*core.User, 0),
+	}
+}
---
+++ internal/core/user.go
@@ -0,0 +1,93 @@
+package core
+
+import "errors"
+
+// UserRepository define as operações de persistência de que o domínio precisa.
+type UserRepository interface {
+	// Save salva um usuário no repositório.
+	Save(u *User) (*User, error)
+	// FindById retorna um usuário pelo ID.
+	FindById(id string) (*User, error)
+	// FindByEmail retorna um usuário pelo email.
+	FindByEmail(email string) (*User, error)
+}
+
+// User representa um usuário no sistema.
+// Nossa classe de domínio.
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	IsActive bool   `json:"is_active"`
+}
+
+// NewUser cria um novo usuário com os dados fornecidos.
+// Retorna um ponteiro para um novo User com os campos vazios.
+func NewEmptyUser() *User {
+	return &User{}
+}
+
+// NewUser cria um novo usuário com os dados fornecidos.
+// Retorna um ponteiro para um novo User com os campos preenchidos.
+func NewUser(email, name string) *User {
+	return &User{
+		Email: email,
+		Name:  name,
+	}
+}
+
+// UserService define as que sao expostas para o mundo externo.
+type UserService interface {
+	// CreateUser cria um novo usuário no sistema.
+	CreateUser(u *User) (*User, error)
+	// GetUser retorna um usuário pelo ID.
+	GetUser(id string) (*User, error)
+	// ActivateUser ativa um usuário pelo ID.
+	ActivateUser(id string) error
+}
+
+// userService implementa as operações de serviço para usuários.
+// Ele atua como intermediário entre o repositório e o mundo externo.
+type userService struct {
+	repo UserRepository
+}
+
+func NewUserService(r UserRepository) UserService {
+	return &userService{repo: r}
+}
+
+func (s *userService) CreateUser(u *User) (*User, error) {
+	if u.Email == "" {
+		return nil, errors.New("email é obrigatório")
+	}
+	if u.Name == "" {
+		return nil, errors.New("name é obrigatório")
+	}
+
+	return s.repo.Save(u)
+}
+
+func (s *userService) GetUser(id string) (*User, error) {
+	return s.repo.FindById(id)
+}
+
+func (s *userService) ActivateUser(id string) error {
+	user, err := s.GetUser(id)
+	if err != nil {
+		return err
+	}
+
+	if user.IsActive {
+		return errors.New("user ja esta ativo")
+	}
+
+	user.IsActive = true
+
+	_, err = s.repo.Save(user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
