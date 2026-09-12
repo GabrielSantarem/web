57cb22c4ad818c76f470d4312f950b631d4f7113
MrTomatePNG <mrtomate360@outlook.com>
2026-09-11 22:20:47 -0400

    Add validated user creation request type

--- handler/types.go
+++ handler/types.go
@@ -1,5 +1,7 @@
 package handler
 
+import "codeberg.org/MrTomate/web/internal/core"
+
 // Message representa a resposta simples do endpoint hello.
 type Message struct {
 	Message string `json:"message"`
@@ -9,4 +11,16 @@ type Message struct {
 // Me representa a resposta com o nome extraído da rota.
 type Me struct {
 	Name string `json:"name"`
+}
+
+type UserCreateRequest struct {
+	Name  string `json:"name" validate:"required,min=3,max=50"`
+	Email string `json:"email" validate:"required,email"`
+}
+
+func (r *UserCreateRequest) ToUser() *core.User {
+	return &core.User{
+		Name:  r.Name,
+		Email: r.Email,
+	}
 }
