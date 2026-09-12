0ac0f5a2eea8f875a9af3ec0a64c0c39f8dc846b
MrTomatePNG <mrtomate360@outlook.com>
2026-09-11 22:20:56 -0400

    Implement user creation handler

--- handler/user.go
+++ handler/user.go
@@ -1,9 +1,46 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func HandleCreateUser() http.Handler {
+	"codeberg.org/MrTomate/web/internal/core"
+	"github.com/go-playground/validator/v10"
+)
+
+var validatorInstance = validator.New()
+
+func HandleCreateUser(srv core.UserService) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Create User"))
+		var newUser UserCreateRequest
+
+		err := json.NewDecoder(r.Body).Decode(&newUser)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": err.Error(),
+			})
+			return
+		}
+		if err = validatorInstance.Struct(newUser); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		user, err := srv.CreateUser(newUser.ToUser())
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{
+				"error": err.Error(),
+			})
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(user)
 	})
 }
