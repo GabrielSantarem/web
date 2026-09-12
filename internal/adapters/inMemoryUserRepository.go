674fa4ff3ccbd29fa9d08de49d4c1c26d6dffd13
MrTomatePNG <mrtomate360@outlook.com>
2026-09-12 17:20:14 -0400

    verificação de duplicidade de Email
    antes de inserir, checa se nao existe um user no array com esse email,
    se existir retorna um `core.ErrEmailAlreadyUsed`.

--- internal/adapters/inMemoryUserRepository.go
+++ internal/adapters/inMemoryUserRepository.go
@@ -1,6 +1,7 @@
 package adapters
 
 import (
+	"slices"
 	"strconv"
 
 	"codeberg.org/MrTomate/web/internal/core"
@@ -13,10 +14,19 @@ type InMemoryUserRepository struct {
 func (r *InMemoryUserRepository) CreateUser(u *core.User) (*core.User, error) {
 	id := len(r.users)
 	u.ID = strconv.Itoa(id + 1)
+
+	i :=  slices.IndexFunc(r.users,func(e *core.User) bool {
+		return e.Email == u.Email
+	})
+	
+	if i != -1 {
+		return nil, core.ErrEmailAlreadyUsed
+	}
+	
 	r.users = append(r.users, u)
 	return u, nil
 }
-
+ 
 func (r *InMemoryUserRepository) UpdateUser(u *core.User) (*core.User, error) {
 	for i, user := range r.users {
 		if user.Email == u.Email && user.ID == u.ID {
