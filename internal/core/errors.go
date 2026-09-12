6f6bd5e1120d2636a6919aca624665d03544303a
MrTomatePNG <mrtomate360@outlook.com>
2026-09-12 14:06:25 -0400

    Criando Sentilas pra valores de erro
    Fica mais facil fazer asserts e determinar o erro usando `errors.Is()`

---
+++ internal/core/errors.go
@@ -0,0 +1,10 @@
+package core
+
+import "errors"
+
+var (
+	ErrUserNotFound      = errors.New("user não encontrado")
+	ErrEmailAlreadyUsed  = errors.New("email já cadastrado")
+	ErrUserAlreadyActive = errors.New("user já está ativo")
+	ErrInvalidInput      = errors.New("entrada inválida")
+)
