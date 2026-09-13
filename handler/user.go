931fcfc4e0becd1e9978ff1612b7e1379efed35b
MrTomatePNG <mrtomate360@outlook.com>
2026-09-13 11:34:31 -0400

    utilização de errors.Is

--- handler/user.go
+++ handler/user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"codeberg.org/MrTomate/web/internal/core"
@@ -9,36 +10,47 @@ import (
 
 func HandleCreateUser(srv core.UserService) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var newUser UserCreateRequest
-
-		err := json.NewDecoder(r.Body).Decode(&newUser)
-
 		w.Header().Set("Content-Type", "application/json")
 
-		if err != nil {
+		var newUser UserCreateRequest
+		if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": err.Error(),
+			json.NewEncoder(w).Encode(ErrorResponse{
+				Message: "json invalido",
+				Err:     err.Error(),
 			})
 			return
 		}
-		if err = validatorInstance.Struct(newUser); err != nil {
+
+		if err := validatorInstance.Struct(newUser); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": err.Error(),
+			json.NewEncoder(w).Encode(ErrorResponse{
+				Message: ErrValidationFailure.Error(),
+				Err:     ErrValidationFailure.Error(),
+				Errors:  formatValidationError(err),
 			})
 			return
 		}
 
 		user, err := srv.CreateUser(newUser.ToUser())
-
 		if err != nil {
+			if errors.Is(err, core.ErrUserAlreadyExists) {
+				w.WriteHeader(http.StatusConflict)
+				json.NewEncoder(w).Encode(ErrorResponse{
+					Message: "email ja cadastrado",
+					Err:     err.Error(),
+				})
+				return
+			}
+
 			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": err.Error(),
+			json.NewEncoder(w).Encode(ErrorResponse{
+				Message: "erro interno no servidor",
+				Err:     err.Error(),
 			})
 			return
 		}
+
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(user)
 	})
